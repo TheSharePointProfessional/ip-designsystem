d0b6a864aa4b90290def2b6a61c0ddc0eb8555aa
Andrew Petersen <andrew.petersen15@gmail.com>
2019-12-17 13:35:30 -0600

    switched Info to use React.FC and support additionalProps

--- portalsdev-ui-toolkit/src/components/primitives/Info.tsx
+++ portalsdev-ui-toolkit/src/components/primitives/Info.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 
-const CLASS_NAME = "primitive-info";
-export default function Info({ children, className = "", as = "div" }) {
+const CLASS_NAME = "info-ui-toolkit";
+
+export interface InfoProps {
+  className?: string;
+  as?: string;
+  [key: string]: any;
+}
+
+const Info: React.FC<InfoProps> = ({
+  children,
+  className = "",
+  as = "div",
+  ...additionalProps
+}) => {
   let cssClass = [CLASS_NAME, className].filter(Boolean).join(" ");
   return (
-    <StyledInfo as={as} className={cssClass}>
+    <StyledInfo {...additionalProps} as={as} className={cssClass}>
       {children}
     </StyledInfo>
   );
-}
+};
+
+export default Info;
 
 const StyledInfo = styled.div`
   color: ${(props) => props.theme.semanticColors.bodySubtext};
