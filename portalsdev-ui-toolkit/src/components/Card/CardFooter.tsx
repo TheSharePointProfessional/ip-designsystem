4aaf41ebd6f9700b5db0816da38ae2fa8265f05b
Petersen <apetersen@skylinetechnologies.com>
2019-12-17 23:29:38 -0600

    switched to React.FC format

--- portalsdev-ui-toolkit/src/components/Card/CardFooter.tsx
+++ portalsdev-ui-toolkit/src/components/Card/CardFooter.tsx
@@ -2,11 +2,29 @@ import React from "react";
 import styled from "styled-components";
 
 const CLASS_NAME = "card-footer";
-export default function CardFooter({ children, className = "" }) {
-  let cssClass = [CLASS_NAME, className].filter(Boolean).join(" ");
-  return <StyledFooter className={cssClass}>{children}</StyledFooter>;
+
+export interface CardFooterProps {
+  // props
+  className?: string;
+  [key: string]: any;
 }
-const StyledFooter = styled.div`
+
+const CardFooter: React.FC<CardFooterProps> = ({
+  children,
+  className = "",
+  ...additionalProps
+}) => {
+  let cssClass = [CLASS_NAME, className].filter(Boolean).join(" ");
+  return (
+    <StyledCardFooter {...additionalProps} className={cssClass}>
+      {children}
+    </StyledCardFooter>
+  );
+};
+
+export default CardFooter;
+
+const StyledCardFooter = styled.div`
   display: flex;
   justify-content: space-between;
   margin-top: auto !important;
