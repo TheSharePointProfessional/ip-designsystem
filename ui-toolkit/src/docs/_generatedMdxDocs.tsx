e8d362ab9348882f472486bff512a7896750f8ab
Petersen <apetersen@skylinetechnologies.com>
2020-04-21 19:36:26 -0500

    documented usePagedItems

--- ui-toolkit/src/docs/_generatedMdxDocs.tsx
+++ ui-toolkit/src/docs/_generatedMdxDocs.tsx
@@ -16,6 +16,7 @@ import UseAsyncDataDocs, { metadata as UseAsyncDataMetadata } from "ui-toolkit/h
 import UseDebouncedEffectDocs, { metadata as UseDebouncedEffectMetadata } from "ui-toolkit/hooks/useDebouncedEffect.mdx"
 import UseDebouncedValueDocs, { metadata as UseDebouncedValueMetadata } from "ui-toolkit/hooks/useDebouncedValue.mdx"
 import UseIntervalDocs, { metadata as UseIntervalMetadata } from "ui-toolkit/hooks/useInterval.mdx"
+import UsePagedItemsDocs, { metadata as UsePagedItemsMetadata } from "ui-toolkit/hooks/usePagedItems.mdx"
 import UsePagingDocs, { metadata as UsePagingMetadata } from "ui-toolkit/hooks/usePaging.mdx"
 import UsePersistedStateDocs, { metadata as UsePersistedStateMetadata } from "ui-toolkit/hooks/usePersistedState.mdx"
 import UseQueryStringDocs, { metadata as UseQueryStringMetadata } from "ui-toolkit/hooks/useQueryString.mdx"
@@ -139,6 +140,13 @@ const docs = [
         sort: UseIntervalMetadata?.sort ?? 9999,
         parent: UseIntervalMetadata?.parent || "hooks",
         render: () => <UseIntervalDocs />,
+    },
+   {
+        title: "UsePagedItems",
+        section: "hooks",
+        sort: UsePagedItemsMetadata?.sort ?? 9999,
+        parent: UsePagedItemsMetadata?.parent || "hooks",
+        render: () => <UsePagedItemsDocs />,
     },
    {
         title: "UsePaging",
